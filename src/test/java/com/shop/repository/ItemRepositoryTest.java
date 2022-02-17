2d9c79cc80999e6005a9dffe9b7bc168e1586272
bhkim@naver.com <*wnddnjs0>
2022-02-18 06:34:05 +0900

    20220218 repository JPA 쿼리 메소드 추가(테스트 포함)

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,6 +34,53 @@ class ItemRepositoryTest {
 
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    public void createItemList(){
+        for(int i = 0; i < 10; i++){
+            Item item = new Item();
+            item.setItemNm("테스트상품" + i);
+            item.setPrice(100 + i);
+            item.setItemDetail("테스트 상품 설명 " + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNm("테스트상품1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트상품1", "테스트 상품 상세 설명 2");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 price 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(103);
+        //List<Item> itemList = itemRepository.findByPriceLessThanOrderOrderByPriceDesc(103);
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 
 }
