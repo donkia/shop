66ace97bfb36abb3aaa993d817efe2c2a67e2013
bhkim@naver.com <*wnddnjs0>
2022-02-21 06:39:49 +0900

    20220221 스프링시큐리티를 활용한 로그인 페이지 생성(진행 중)

---
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/***
+ * WebSecurityConfigurerAdapter를 상속받는 클래스에 @EnableWebSecurity 선언하면 SpringSecurityFilterChain이 자동으로 포함
+ * WebSecurityConfigurerAdapter를 상속받아서 메서드를 오버라이딩을 통한 보안 설정을 커스터마이징 가능.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /***
+     * http 요청에 대한 보안을 설정
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    /** DB에 패스워드 저장 시 암호화 가능
+     *  BCryptPasswordEncoder의 해시 함수를 이용하여 비밀번호를 암호화하여 저장
+     * */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.shop.service;
+
+import com.shop.dto.MemberRepository;
+import com.shop.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member){
+
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null){
+            throw new IllegalStateException("이미 가입된 회원입니다");
+        }
+
+    }
+}
